4fa8c756acbfbf71c1d7807809e04abbe856a1cd
Tristan FAZZALARI <FAZZALARI@cti.lan>
2019-12-03 16:45:58 +0100

    debut fusion index.php & authenticate.php. Ajout message erreur login

--- authenticate.php
+++ authenticate.php
@@ -5,38 +5,39 @@ require_once('conn.php');
 session_start();
 
 
-if ( !isset($_POST['username'], $_POST['password']) ) {
-    die ('Please fill both the username and password field!');
-}
+if (isset($_POST['username'], $_POST['password'])) {
 
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-	// Bind parameters (s = string, i = int, b = blob, d = décimal), username = string so "s"
-	$stmt->bind_param('s', $_POST['username']);
-	$stmt->execute();
-	// Store the result so we can check if account exists in database.
-	$stmt->store_result();
-}
 
-if ($stmt->num_rows > 0) {
-	$stmt->bind_result($id, $password);
-	$stmt->fetch();
-	// Account exists, now we verify the password.
-	// Note: remember to use password_hash in your registration file to store the hashed passwords.
-	if ($_POST['password'] === $password) {
-		// Verification success! User has loggedin!
-		// Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
-        session_regenerate_id();
-        
-		$_SESSION['loggedin'] = TRUE;
-		$_SESSION['name'] = $_POST['username'];
-        $_SESSION['id'] = $id;
-        
-		header('Location: home.php'); //Home redirection
-	} else {
-		echo 'Incorrect password!';
-	}
-} else {
-	echo 'Incorrect username!';
-}
 
-$stmt->close();
+    if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+        // Bind parameters (s = string, i = int, b = blob, d = décimal), username = string so "s"
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        // Store the result so we can check if account exists in database.
+        $stmt->store_result();
+    }
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now check password.
+
+        if ($_POST['password'] === $password) {
+            // Success!
+            // Create sessions so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            session_regenerate_id();
+
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+
+            header('Location: home.php'); //Home redirection
+        } else {
+            $pb = "Incorrect password!";
+        }
+    } else {
+        $pb = "Incorrect username!";
+    }
+
+    $stmt->close();
+}
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('authenticate.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Login page</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="login">
+      <h1>Login</h1>
+      <p class="problem"> <?php if (isset($pb)) echo $pb; ?> </p>
+      <form method="POST" action="index.php">
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          required="required"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required="required"
+        />
+        <button type="submit" class="btn btn-primary btn-block btn-large">
+          Let me in.
+        </button>
+      </form>
+      <br>
+      <span id="noacc" onclick="document.location.href='register.html'" >No account yet ? Register here.</span> 
+    </div>
+  </body>
+</html>
